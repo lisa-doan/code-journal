d7c25abe9dd2dcaa2d6abc404b471b18bbdbac51
Lisa Doan <ldoan24@gmail.com>
2021-02-09 14:56:07 -0800

    prepend new object, reset src attribute & form input.

--- js/main.js
+++ js/main.js
@@ -1,27 +1,23 @@
 /* global data */
 /* exported data */
-
-// Listen for 'input' events on the photoUrl input to update the src attribute of the photo preview when the input value changes.
 var $photoUrl = document.querySelector('#photoUrl');
+var $form = document.querySelector('form');
+var $imgHolder = document.querySelector('.image');
+
 $photoUrl.addEventListener('input', function (event) {
-  if (event.target.id === 'photoURL') {
-    var $imgHolder = document.querySelector('.image');
-    $imgHolder.getAttribute('src', event.target.value);
-  }
+  $imgHolder.setAttribute('src', event.target.value);
 });
 
-var $form = document.querySelector('form');
 $form.addEventListener('submit', function (event) {
   event.preventDefault();
   var input = {
-    photoUrl: event.target.photoUrl.value,
+    image: event.target.photoUrl.value,
     title: event.target.title.value,
     notes: event.target.notes.value
   };
-  var i = 1;
-  input.nextEntryId = i++;
-
-  // console.log(input);
+  input.entryId = data.nextEntryId;
+  data.nextEntryId++;
+  $imgHolder.setAttribute('src', 'images/placeholder-image-square.jpg');
   $form.reset();
 
 });
