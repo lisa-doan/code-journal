8ad84985f4a50808a70aeb8cee57897ebdd71fa6
Lisa Doan <ldoan24@gmail.com>
2021-02-10 15:23:14 -0800

    added a function to created a DOM tree when DOMContentLoaded is fired

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 var $photoUrl = document.querySelector('#photoUrl');
 var $form = document.querySelector('form');
 var $imgHolder = document.querySelector('.image');
+var $entriesList = document.querySelector('.entries-list');
 
 $photoUrl.addEventListener('input', function (event) {
   $imgHolder.setAttribute('src', event.target.value);
@@ -20,4 +21,41 @@ $form.addEventListener('submit', function (event) {
   data.entries.unshift(input);
   $imgHolder.setAttribute('src', 'images/placeholder-image-square.jpg');
   $form.reset();
+});
+
+function createEntry(entry) {
+  var $li = document.createElement('li');
+
+  var $row = document.createElement('div');
+  $row.setAttribute('class', 'row');
+  $li.appendChild($row);
+
+  var $columnHalfLeft = document.createElement('div');
+  $columnHalfLeft.setAttribute('class', 'column-half');
+  $row.append($columnHalfLeft);
+
+  var $entryImage = document.createElement('img');
+  $entryImage.setAttribute('src', entry.image);
+  $columnHalfLeft.appendChild($entryImage);
+
+  var $columnHalfRight = document.createElement('div');
+  $columnHalfRight.setAttribute('class', 'column-half');
+  $row.append($columnHalfRight);
+
+  var $entryTitle = document.createElement('h2');
+  $entryTitle.textContent = entry.title;
+  var $entryNotes = document.createElement('p');
+  $entryNotes.textContent = entry.notes;
+
+  $columnHalfRight.appendChild($entryTitle);
+  $columnHalfRight.appendChild($entryNotes);
+
+  return $li;
+}
+
+window.addEventListener('DOMContentLoaded', function (event) {
+  for (var i = 0; i < data.entries.length; i++) {
+    var entry = createEntry(data.entries[i]);
+    $entriesList.appendChild(entry);
+  }
 });
